78d10b6258f213baa534f7f511feb757cc4c01d7
Hitesh Kumar <hiteshkumar@hiteshs-mbp.home>
2025-03-18 18:04:55 -0400

    Configuring query & resolvers with dummy db.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import {typeDefs} from './schema';
+import { typeDefs } from './schema.js';
+import _db from './_db.js';
+
 const PORT = 4000;
 
+const resolvers = {
+    Query: {
+        games() {
+            return _db.games
+        },
+        authors() {
+            return _db.games
+        },
+        reviews() {
+            return _db.reviews
+        }
+    }
+}
 const server = new ApolloServer({
     typeDefs, // The schema
     resolvers // handler for incoming requests
